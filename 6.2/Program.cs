4621e9c9e66f919399491c5f73563cbb88d9af14
Aramayis Gyulbekyan <aramayis.gyulbekyan@coafkids.org>
2020-09-04 14:39:56 +0400

    6 homework

---
+++ 6.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = 8;
+            int N = 7;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine(K);
+            }
+            Console.ReadKey();
+        }
+    }
+}
